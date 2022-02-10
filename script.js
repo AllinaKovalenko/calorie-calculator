d9ee0307f8df1a2849f7e05f9a8d4b13e76dfa1e
AllinaKovalenko <a.skripnikov@b2b-center.ru>
2022-02-10 22:43:28 +0300

    Add gitignore file

---
+++ script.js
@@ -0,0 +1,9 @@
+function ageHandler() {
+   return document.querySelector("#age").value;
+}
+function growthHandler(){
+    return document.querySelector("#growth").value;
+}
+function weightHandler(){
+    return document.querySelector("#weight").value;
+}
